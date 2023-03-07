85f18b5d84edaef5f73f3011bd4b9ea3b0f985d2
Yukiyaya098 <126988037+Yukiyaya098@users.noreply.github.com>
2023-03-07 00:24:47 +0000

    Add files via upload

--- do_KNN.m
+++ do_KNN.m
@@ -4,9 +4,10 @@ load 'monkeydata_training.mat';
 
 %Split dataset into train and test
 trial_num = size(trial,1);
-train_pertentage = 0.8;
+train_pertentage = 0.7;
 train_num = round(train_pertentage*size(trial,1));
 temp = randperm(trial_num);
+NN_vec=1:10:261;
 
 train_index = temp(1:train_num);
 train_data=trial(train_index,:);
@@ -18,5 +19,15 @@ else
     test_data=[];
 end
 
+[X,l,~,mX]=Exfeature(train_data);
+[Xt,l_test,~,~]=Exfeature(test_data);
+
+pred=kNN_kernal(2,Xt,mX,1:8,1);
+pred=[pred; kNN_kernal(2,Xt,X,l,NN_vec)];
+
+acc0=100*(mean(pred==l_test,2));
+
+figure;
+plot(acc0)
 
 
