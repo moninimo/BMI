93cebdca9e3647c5fd3e5fd7cd2db4ca18692ff4
Estella-7 <67856150+Estella-7@users.noreply.github.com>
2023-03-10 15:24:15 +0000

    Add files via upload

--- continuousEstimator0.m
+++ continuousEstimator0.m
@@ -34,14 +34,20 @@
 % Make sure that the output contains only the x and y coordinates of the
 % monkey's hand.
 
+accuracy = positionEstimatorTraining('monkeydata_training.mat');
 
-function [modelParameters] = positionEstimatorTraining(training_data)
+figure(1);
+plot(accuracy)
+xlabel('Test Index','FontSize',15)
+ylabel('Accuracy Percentage','FontSize',15)
+title('Accuracy vs Test Index','FontSize',18)
+
+function [accuracy] = positionEstimatorTraining(training_data)
   % Arguments:
-  
   % - training_data:
   %     training_data(n,k)              (n = trial id,  k = reaching angle)
   %     training_data(n,k).trialId      unique number of the trial
-  %     training_data(n,k).spikes(i,t)  (i = neuron id, t = time)
+  %     training_data(n,k).spikes(i,t)  (i Z= neuron id, t = time)
   %     training_data(n,k).handPos(d,t) (d = dimension [1-3], t = time)
   
   % ... train your model
@@ -51,10 +57,44 @@ function [modelParameters] = positionEstimatorTraining(training_data)
   % - modelParameters:
   %     single structure containing all the learned parameters of your
   %     model and which can be used by the "positionEstimator" function.
-  
+  % Clear workplace 
+    
+    % Load data
+    load (training_data);
+    
+    %Get the number of trial 
+    trial_num = size(trial,1);
+    
+    %Split the data to training data and expected data
+    train_pertentage = 0.7;
+    train_num = round(train_pertentage*size(trial,1));
+    temp = randperm(trial_num);
+    NN_vec=1:10:261;
+    
+    %Get the corresponding train data
+    train_index = temp(1:train_num);
+    train_data=trial(train_index,:);
+    
+    % Update test data
+    if train_num < trial_num
+        test_index=temp((train_num+1):end);
+        test_data = trial(test_index,:);
+    else
+        test_data=[];
+    end
+    
+    % 
+    [x,l,~,m_x] = get_feature(train_data);
+    [x_t,l_test,~,~] = get_feature(test_data);
+    
+    predict_data = kNN_kernal(x_t,m_x,1:8,4);
+    predict_data = [predict_data; kNN_kernal(x_t,x,l,NN_vec)];
+    
+    accuracy = 100*(mean(predict_data==l_test,2));
+
 end
 
-function [x, y] = positionEstimator(test_data, modelParameters)
+function [x, y] = positionEstimator(test_data, train_data)
 
   % **********************************************************
   %
@@ -93,14 +133,12 @@ function [x, y] = positionEstimator(test_data, modelParameters)
   %             test_data.decodedHandPos = [2.3; 1.5]
   %             test_data.spikes = 98x340 matrix of spiking activity
   
-  
-  
   % ... compute position at the given timestep.
   
   % Return Value:
   
   % - [x, y]:
   %     current position of the hand
-   
+
 end
 
